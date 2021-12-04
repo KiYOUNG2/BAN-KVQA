9ca345c345f3e73580ed5cb81b80a1008d81ff16
eliza.dukim <eliza.dukim@gmail.com>
2021-12-04 08:55:35 +0000

    Etc update *.sh files

--- tools/download.sh
+++ tools/download.sh
@@ -14,16 +14,32 @@ DATAPATH=data
 
 mkdir -p $DATAPATH
 cd $DATAPATH
-download_gg_large "1gpOaOl0BcUvYpgoOA2JpZY2z-BUhuBLX" word-embeddings.zip
-download_gg_large "0B0ZXk88koS2KbDhXdWg1Q2RydlU" ko.zip
-wget "https://www.dropbox.com/s/stt4y0zcp2c0iyb/ko.tar.gz?dl=1" -O ko.tar.gz
+download_gg_large "1Jdkrq_gH0R5sGOYhXuhSLgQbjPCgDGGw" KVQA_annotations_val.json
+download_gg_large "1vQXgKcIu6v9GBGIiKDa-L0FnimdiUAj_" KVQA_annotations_train.json
+download_gg_large "1DmCALEiKyPV58kBXb4mxsUQBSaFxotbI" KVQA_annotations_test.json
+download_gg_large "1OhDQSMxQcLrYEZAsV10Ik7wZrC835Wyj" check_kvqa.py
 
+# below 2 files(word-embeddings.zip, ko.zip) needs to be downloaded manually
+# download_gg_large "1gpOaOl0BcUvYpgoOA2JpZY2z-BUhuBLX" word-embeddings.zip
 # ratsgo
-unzip word-embeddings.zip
+# unzip word-embeddings.zip
+
+# download_gg_large "0B0ZXk88koS2KbDhXdWg1Q2RydlU" ko.zip
+wget "https://www.dropbox.com/s/stt4y0zcp2c0iyb/ko.tar.gz?dl=1" -O ko.tar.gz
 
 # Kyubong Park
 mkdir -p fasttext
 tar -zxvf ko.tar.gz -C fasttext
-unzip ko.zip -d word2vec
+# unzip ko.zip -d word2vec
 
+# mkdir -p features
+cd features
+# Vizwiz
+# https://drive.google.com/file/d/1sdCbVDnt8jVlGVhm89NbkWMjZE-INoKN/view?usp=sharing
+download_gg_large "19lKxKb5pQtYuICNThAtxBsUWfAgr7Jdy" VizWiz_resnet101_faster_rcnn_genome.tsv
+# KVQA
+# https://drive.google.com/file/d/19lKxKb5pQtYuICNThAtxBsUWfAgr7Jdy/view?usp=sharing
+download_gg_large "19lKxKb5pQtYuICNThAtxBsUWfAgr7Jdy" KVQA_resnet101_faster_rcnn_genome.tsv
+
+cd ..
 cd ..
--- tools/process.sh
+++ tools/process.sh
@@ -4,7 +4,7 @@
 ## Process data
 ## Notice that 10-100 adaptive bottom-up attention features are used.
 python tools/compute_softscore.py
-python tools/create_dictionary.py --embedding glove-rg
-python tools/create_dictionary.py --embedding word2vec-pkb
-python tools/create_dictionary.py --embedding fasttext-pkb
+# python tools/create_dictionary.py --embedding glove-rg
+# python tools/create_dictionary.py --embedding word2vec-pkb
+# python tools/create_dictionary.py --embedding fasttext-pkb
 python tools/adaptive_detection_features_converter.py
