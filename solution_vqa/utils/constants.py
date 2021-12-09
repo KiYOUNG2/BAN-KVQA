a28a35f5518d22f04250d65f2d90fb75a2733690
eliza.dukim <eliza.dukim@gmail.com>
2021-12-09 04:53:44 +0000

    BREAKING CHANGE: mkdir `soltuion_vqa/utils` files: `registry.py`, `constants.py`, `utils.py`

---
+++ solution_vqa/utils/constants.py
@@ -0,0 +1,21 @@
+import os
+
+# Object Detection Model
+VISION_COLUMN_NAME = "v_feature"
+SPATIAL_COLUMN_NAME = "spatials"
+QUESTION_COLUMN_NAME = "question"
+ANSWERABLE_COLUMN_NAME = "answerable"
+# ====================================================
+DETECTOR_ROOT_PATH = './bottom_up_attention_pytorch/'
+DATA_PATH = os.path.join(DETECTOR_ROOT_PATH, 'evaluation')
+DETECTOR_CONFIG_FILE_PATH = os.path.join(DETECTOR_ROOT_PATH, 'configs/bua-caffe/extract-bua-caffe-r101.yaml')
+OBJECT_VOCAB_FILE = 'objects_vocab.txt'
+ATTR_VOCAB_FILE = 'attributes_vocab.txt'
+DETECTOR_MIN_BOXES = 10
+DETECTOR_MAX_BOXES = 30
+DETECTOR_CONF_THRESH = 0.4
+
+# VQA Model
+VQA_CONFIG_FILE_PATH = './configs/vqa.yaml'
+LABEL2ANS_FILE = './data/cache/trainval_label2ans.kvqa.pkl'
+ANS2LABEL_FILE = './data/cache/trainval_label2ans.kvqa.pkl'
