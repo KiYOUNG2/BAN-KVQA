e868c9e2f86249dfb48909dd97de1f49eebb9340
eliza.dukim <eliza.dukim@gmail.com>
2021-12-14 12:17:29 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import glob
+from os.path import basename, splitext
+import pathlib
+from typing import ParamSpecKwargs
+from setuptools import find_packages, setup
+
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# Long description
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+# This call to setup() does all the work
+setup(
+    name="vqa",
+    version="0.0.1",
+    description="Bilinear Attention Networks for Visual Question Anwering",
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KiYOUNG2/BAN-KVQA",
+    author="eliza.dukim",
+    author_email="eliza.dukim@gmail.com",
+    license="GNU",
+
+    packages=find_packages(where=[
+        'bottom_up_attention_pytorch',
+        'bottom_up_attention_pytorch/detectron2'
+        ],
+        ),
+    install_requires=requirements,
+)
